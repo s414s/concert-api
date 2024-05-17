6d0e7169520338bd5c3708d56686d33eb49ea466
Alberto Salas <alsalasher@gmail.com>
2024-05-17 11:42:16 +0200

    add domain entities

---
+++ Domain/Entities/Group.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enum;
+
+namespace Domain.Entities;
+
+public class Group
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public bool ExplicitContent { get; set; }
+    public Genre Genre { get; set; }
+}
---
+++ Domain/Enum/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Enum;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Genre
+{
+    Rock = 1,
+    Pop = 2,
+    Rap = 3,
+}
