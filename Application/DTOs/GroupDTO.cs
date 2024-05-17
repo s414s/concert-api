3299e70f7f1a5919bfc2850543de353612f392f0
Carlota Cetina <carlotika26@gmail.com>
2024-05-17 15:32:00 +0200

    Add event controller

--- Application/DTOs/GroupDTO.cs
+++ Application/DTOs/GroupDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace Application.DTOs;
+﻿using Domain.Enum;
+
+namespace Application.DTOs;
 
 public class GroupDTO
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
+    public string? Password { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public bool ExplicitContent { get; set; }
+    public Genre Genre { get; set; }
 }
--- WebApi/Controllers/EventsController.cs
+++ WebApi/Controllers/EventsController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using CliniCareApp.Business;
-using CliniCareApp.Models;
 using Microsoft.AspNetCore.Cors;
+using Application.Contracts;
+using Application.DTOs;
 
 namespace WebApi.API.Controllers;
 
@@ -11,24 +11,24 @@ namespace WebApi.API.Controllers;
 public class EventsController : ControllerBase
 {
     private readonly ILogger<EventsController> _logger;
-    private readonly IEventsService _patientService;
+    private readonly IEventServices _eventService;
 
 
-    public EventsController(ILogger<EventsController> logger, IEventService eventService)
+    public EventsController(ILogger<EventsController> logger, IEventServices eventService)
     {
         _logger = logger;
-        _eventService = EventService;
+        _eventService = eventService;
     }
 
 
     [HttpGet(Name = "GetAllEvents")] 
-    public ActionResult<IEnumerable<Events>> GetAllEvents([FromQuery] EventQueryParameters eventQueryParameters)
+    public ActionResult<IEnumerable<Application.DTOs.EventDTO>> GetAllEvents()
     {
         if (!ModelState.IsValid)  {return BadRequest(ModelState); } 
 
         try 
         {
-            var events = _eventService.GetAllEvents(eventQueryParameters);
+            var events = _eventService.GetEvents();
             
                 if (events == null || !events.Any())
                     {
@@ -44,19 +44,77 @@ public class EventsController : ControllerBase
     }
 
     [HttpGet("{eventId}", Name = "GetEventById")]
-    public IActionResult GetEvent(int eventId)
+    public IActionResult GetEventById(long eventId)
     {
         try
         {
-            var event = _eventService.GetEventById(eventId);
-            return Ok(event);
+            var e = _eventService.GetEventById(eventId);
+            return Ok(e);
            
         }
         catch (KeyNotFoundException)
         {
-            return NotFound($"No existe eventos con el Id {Id}");
+            return NotFound($"No existe eventos con el Id {eventId}");
         }
     }
+
+
+    [HttpPost]
+    public IActionResult NewEvent([FromBody] EventDTO eventDto)
+    {
+        // Verificar si el modelo recibido es válido
+        if (!ModelState.IsValid){ return BadRequest(ModelState);}
+
+        if (string.IsNullOrEmpty(eventDto.Name) || string.IsNullOrEmpty(eventDto.ZipCode))
+
+        {
+            return BadRequest("Los campos no pueden estar vacíos.");
+        }
+
+        try 
+        {
+            var ev = _eventService.CreateEvent(eventDto);
+            return CreatedAtAction(nameof(GetAllEvents), new { eventId = ev.Id }, ev);
+        }     
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+
+    [HttpPut("{eventId}")]
+    public IActionResult UpdateEvent(int eventId, [FromBody] EventDTO eventDto)
+    {
+        if (!ModelState.IsValid)  {return BadRequest(ModelState); } 
+
+        try
+        {
+            _eventService.UpdateEvent(eventId, eventDto);
+            return Ok($"El evento con Id: {eventId} ha sido actualizado correctamente");
+        }
+        catch (KeyNotFoundException)
+        {
+           return NotFound();
+        }
+    }
+
+
+    [HttpDelete("{eventId}")]
+    public IActionResult DeleteEvent(int eventId)
+    {
+        try
+        {
+            _eventService.DeleteEvent(eventId);
+            return Ok($"El evento con Id: {eventId} ha sido borrado correctamente");
+        }
+        catch (KeyNotFoundException ex)
+        {
+            _logger.LogInformation(ex.Message);
+            return NotFound();
+        }
+    }
+
 }
 
    
