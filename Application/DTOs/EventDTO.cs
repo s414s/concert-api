7331906fcaadc27f9302a1420582c4fb3fe2ddaf
Carlota Cetina <carlotika26@gmail.com>
2024-05-17 17:56:03 +0200

    Update readme

--- Application/DTOs/EventDTO.cs
+++ Application/DTOs/EventDTO.cs
@@ -4,6 +4,7 @@ public class EventDTO
 {
     public long Id { get; set; }
     public string Name { get; set; }
+    public DateTime Date { get; set; }
     public string ZipCode { get; set; }
     public int Capacity { get; set; }
     public bool OnlyAdults { get; set; }
